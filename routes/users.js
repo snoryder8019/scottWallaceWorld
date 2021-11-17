cd0123282d6a538dfb4d0702c769f9dd7ebc4d97
snoryder8019 <snoryder8019@gmail.com>
2021-11-17 08:56:44 -0700

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router =express.Router();
+//const User = require("../models/users.js");
+const User = require("../models/user.js");
+router.get('/login',(req,res)=>{
+  res.render('login');
+})
+//
+router.get('/register',(req,res)=>{
+res.render('register');
+})
+//
+router.post('/register',(req,res)=>{
+
+
+
+const {name,email, password, password2} = req.body;
+let errors = [];
+console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
+if(!name || !email || !password || !password2) {
+    errors.push({msg : "Please fill in all fields"})
+}
+if(password !== password2) {
+    errors.push({msg : "passwords dont match"});
+}
+if(password.length < 6 ) {
+    errors.push({msg : 'password atleast 6 characters'})
+}
+
+
+
+if(errors.length > 0 ) {
+res.render('register', {
+    errors : errors,
+    name : name,
+    email : email,
+    password : password,
+    password2 : password2})
+} else {
+    //validation passed
+    // User.findOne({email : email}).exec((err,user)=>{
+    // console.log(user);
+
+if(user) {
+       errors.push({msg: 'email already registered'});
+       render(res,errors,name,email,password,password2);
+
+      } else {
+       const newUser = new User({
+           name : name,
+           email : email,
+           password : password
+      });
+      //hash password
+            // bcrypt.genSalt(10,(err,salt)=>
+            // bcrypt.hash(newUser.password,salt,
+            //     (err,hash)=> {
+            //         if(err) throw err;
+            //             //save pass to hash
+            //             newUser.password = hash;
+            //         //save user
+            //         newUser.save()
+            //         .then((value)=>{
+            //             console.log(value)
+            //         res.redirect('/users/login');
+            //         })
+            //         .catch(value=> console.log(value));
+            //
+            //     }));
+          };
+
+})
+router.post('/login',(req,res)=>{})
+router.get('/logout', (req,res)=>{})
+module.exports=router;
