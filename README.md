4b2f31d3782ce5bbe9be294b771b6af2e08045cb
snoryder8019 <snoryder8019@gmail.com>
2022-01-04 22:28:16 -0700

    button styles~ AE sandbox

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # scottWallaceWorld
+
 my in development portfolio. My third level of priorities
 
 This is my virtual playground.
 
 www.scottwallace.world
 
-I will be adding to the sandbox frequently
+I will be adding to the sandbox frequently 1-4-22
 
-This is a clone from my two live websites. Most experimenting and implimentations are performed here before updating Valrick and BALC.
+This is a clone from my two live websites. Most experimenting and implementations are performed here before updating to Valrick and BALC.
 
 More to come on my readme.md
--- public/jsFiles/menu.js
+++ public/jsFiles/menu.js
@@ -1,46 +1,79 @@
 const menu = document.getElementById('menu');
 const resume = document.getElementById('resPage');
-  const colorDiv = document.getElementById('colorDiv');
-var pageHandle = [resume,0];
+const colorDiv = document.getElementById('colorDiv');
+const buttonGlobe = document.getElementById('buttonGlobe');
+const loadScreen = document.getElementById('loadScreen');
+const loadDelay = 5000;
+//var pageHandle = [resume,0];
 // function pageCloser(){
 //   for(x=0;x<pageHandle.length;x++){
 //     pageHandle[x].style.opacity="0";
 //   }
 // };
-function popLoader(){
-  var inH = window.innerHeight;
-  var inW = window.innerWidth;
-  const e = window.event;
-colorDiv.addEventListener('mousemove',function(){
-var x= window.event.screenX;
-var y= window.event.screenY;
-var mapX =(x/inW);
-var mapY =(y/inH);
-console.log(mapX+" "+mapY)
-colorDiv.style.background='linear-gradient(to left, rgba('+135+','+y/3+','+x/2+'), rgba('+x+',200,'+y/3+'))'
-});
+
+const delayedAnnie = function delayedAnnie0(){
+
+  buttonGlobe.style.display='block';
+  loadScreen.style.display='none';
+};
+//
+//controls loading spinner
+function loadScreenOn(){
+  console.log('loadScreenON');
+  if(loadScreen===null){}else{
+  loadScreen.style.display='block';
+buttonGlobe.style.display='none';
+setTimeout(delayedAnnie,loadDelay);
 }
+};
+//
+//controls masking gradients in sandbox
+function maskingFunc(){
+  if(colorDiv === null){}
+  else{
+    var inH = window.innerHeight;
+    var inW = window.innerWidth;
+    const e = window.event;
+  colorDiv.addEventListener('mousemove',function(){
+  let x= window.event.screenX;
+  let y= window.event.screenY;
+   let mapX =(x/inW);
+   let mapY =(y/inH);
+  console.log(mapX+" "+mapY);
+  colorDiv.style.background='linear-gradient(to left, rgba('+135+','+y/3+','+x/2+'), rgba('+x+',200,'+y/3+'))'
+  });}
+
+}
+
 
 function menuOpen(){
   if (menu.style.left!='0vw'){
-    console.log('if');
+    console.log('if menuOpen()');
     menu.style.left='0vw'
     }
 else{
-  console.log('else')
+  console.log('else menuOpen()')
   menu.style.left='-100vw'
 }
 };
 
 function resPage(){
+  console.log('resPage')
   if (resume.style.opacity!='100'){
-    console.log('if Res');
+    console.log('if resPage()');
       menuOpen();
   resume.style.display="block";
   resume.style.opacity="100";
   }
   else{
     resume.style.opacity="0";
-    console.log('else Res');
+    console.log('else resPage()');
   }
 };
+//
+//event listeners called "onload" in the body
+function popLoader(){
+  loadScreenOn();
+  maskingFunc();
+console.log('listeners Added')
+}
