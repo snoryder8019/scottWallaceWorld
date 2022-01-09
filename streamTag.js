6b68d84de47e32b54974f70ef579c703a4640e3e
snoryder8019 <snoryder8019@gmail.com>
2022-01-09 09:43:08 -0700

    removing unec files

--- streamTag.js
+++ streamTag.js
@@ -2,17 +2,19 @@
 console.log('the bot has started');
 var Twit = require('twit');
 var fs = require('fs');
+const dotenv = require('dotenv').config();
+
 var T = new Twit({
-  consumer_key:         'bfmVy4XOd1KwLmfcYTdv8XxcL',
-  consumer_secret:      '17A2VQh2Ty2BV5CBz9bY104gq34Oth0keMH3QsF2qlh6XWmx4v',
-  access_token:         '194026618-MQ01NklkkNOlsR6ciIiZRhIuLlKH2s5qdciVtQJg',
-  access_token_secret:  'vrKD7vYlrkKEBJwZMBy8un0QDDvdwohfY1h9CvLPuXch0',
+  consumer_key:         process.env.TWIT_KEY,
+  consumer_secret:      process.env.TWIT_SECRET,
+  access_token:         process.env.TWIT_TOKEN,
+  access_token_secret:  process.env.TWIT_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
 //config search parameters below
-var status1 = "rvn"
-var status2 = "ravencoin"
+var status1 = "#getupgreeley"
+var status2 = "greeleyunexpected"
 var stream = T.stream('statuses/filter', { track: status1 && status2 ,language: 'en' })
 
 stream.on('tweet', function (tweet) {
@@ -25,12 +27,3 @@ stream.on('tweet', function (tweet) {
 
 })
 console.log('twitter Stream is now open!!!')
-/*API:
-bfmVy4XOd1KwLmfcYTdv8XxcL
-API Secret:
-17A2VQh2Ty2BV5CBz9bY104gq34Oth0keMH3QsF2qlh6XWmx4v
-
-access:
-194026618-MQ01NklkkNOlsR6ciIiZRhIuLlKH2s5qdciVtQJg
-access secret:
-vrKD7vYlrkKEBJwZMBy8un0QDDvdwohfY1h9CvLPuXch0*/
