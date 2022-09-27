61943d93bd07a380a2d4282c8147cfd502b1b69a
snoryder8019 <snoryder8019@gmail.com>
2022-09-27 10:04:21 -0600

    links and google ads

--- routes/links.js
+++ routes/links.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-
-
-
 router.get('/blc',(req,res)=>{
     console.log("outbound count function needed BALC")
 res.redirect('http://www.badassladiesclub.com')});
+
 router.get('/swlc',(req,res)=>{
 res.redirect('https://www.somewherelandingcabins.com')});
 router.get('/w2',(req,res)=>{
 res.redirect('http://www.w2marketing.biz')});
 router.get('/sook',(req,res)=>{
 res.redirect('http://www.southernoklahomapride.org')});
+
 router.get('/zeroGravity',(req,res)=>{
     res.redirect('http://www.zerogravityinflatables.com')});
+router.get('/noco',(req,res)=>{
+    res.redirect('http://www.nocometalworkz.com')});
+
     router.get('/github',(req,res)=>{
         res.redirect('http://www.github.com/snoryder8019')});
-module.exports = router;
+
+        module.exports = router;
